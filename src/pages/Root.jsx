e014e550d4251c0d15705815e49843470aa87c7d
Chansoo <devhan1207@gmail.com>
2023-07-09 23:16:43 -0400

    add NavBar using tailwind

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import NavBar from '../components/NavBar';
 import {Outlet} from 'react-router-dom';
 
 export default function Root() {
   return (
     <div>
-      <Navbar />
+      <NavBar />
       <Outlet />
     </div>
   );

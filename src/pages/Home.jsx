2a2175788828cd52460704d14b122e63d4521a80
Chansoo <devhan1207@gmail.com>
2023-07-14 20:51:09 -0400

    added slide photos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,27 +12,57 @@ export default function Home() {
           <img src="/images/electronic-bidet.png" alt="water-filtration" />
         </div>
         <div className='grid'>
-          <div className='pl-10 mb-10'>
-            <Carousel slideInterval={2000}>
+          <div className='pl-10'>
+            <Carousel
+              slideInterval={2000}
+              leftControl={<svg class="h-8 w-8 text-gray-200"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="8" y1="12" x2="16" y2="12" />  <line x1="8" y1="12" x2="12" y2="16" />  <line x1="8" y1="12" x2="12" y2="8" /></svg>}
+              rightControl={<svg class="h-8 w-8 text-gray-200"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="16" y1="12" x2="8" y2="12" />  <line x1="16" y1="12" x2="12" y2="16" />  <line x1="16" y1="12" x2="12" y2="8" /></svg>}
+              className='h-96 border-8 rounded-md text-black'
+            >
               <img
-                alt="..."
-                src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+                className='object-scale-down h-full w-full'
+                alt="slide1"
+                src="/images/slide1.png"
               />
               <img
-                alt="..."
-                src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+                alt="slide2"
+                src="/images/slide2.png"
               />
               <img
-                alt="..."
-                src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                alt="slide3"
+                src="/images/slide3.png"
               />
               <img
-                alt="..."
-                src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+                alt="slide4"
+                src="/images/slide4.png"
               />
               <img
-                alt="..."
-                src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+                alt="slide5"
+                src="/images/slide5.png"
+              />
+              <img
+                alt="slide6"
+                src="/images/slide6.png"
+              />
+              <img
+                alt="slide7"
+                src="/images/slide7.png"
+              />
+              <img
+                alt="slide8"
+                src="/images/slide8.png"
+              />
+              <img
+                alt="slide9"
+                src="/images/slide9.png"
+              />
+              <img
+                alt="slide10"
+                src="/images/slide10.png"
+              />
+              <img
+                alt="slide11"
+                src="/images/slide11.png"
               />
             </Carousel>
           </div>
